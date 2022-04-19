8105ca8ffd711542c2dbc64d43973a92492e3975
natan1563 <natromafe@gmail.com>
2022-04-18 21:22:45 -0300

    Refactoring migrate

--- src/Database/Migrate.js
+++ src/Database/Migrate.js
@@ -4,33 +4,37 @@ const clientTable = require('./Models/Client')
 const addressTable = require('./Models/Address')
 const sellerTable = require('./Models/Seller')
 const logTable = require('./Models/Log')
-
-vehicleTable
-  .sync()
-  .then(() => console.log('Vehicle table has be created'))
-  .catch((e) => console.error('Vehicle table does\'nt be created', e.message))
   
-manufacturerTable
+async function run() {
+  await manufacturerTable
   .sync()
   .then(() => console.log('Manufacturer table has be created'))
   .catch((e) => console.error('Manufacturer table does\'nt be created', e.message))
 
-clientTable
-  .sync()
-  .then(() => console.log('Client table has be created'))
-  .catch((e) => console.error('Client table does\'nt be created', e.message))
+  await addressTable
+    .sync()
+    .then(() => console.info('Address table created with successfuly'))
+    .catch((e) => console.error('Cant be create address table'))
 
-addressTable
-  .sync()
-  .then(() => console.info('Address table created with successfuly'))
-  .catch((e) => console.error('Cant be create address table'))
+  await clientTable
+    .sync()
+    .then(() => console.log('Client table has be created'))
+    .catch((e) => console.error('Client table does\'nt be created', e.message))
 
-sellerTable
-  .sync()
-  .then(() => console.info('The Seller table has be created'))
-  .catch((e) => console.error('Ops! Cant be create seller table', e.message))
+  await vehicleTable
+    .sync()
+    .then(() => console.log('Vehicle table has be created'))
+    .catch((e) => console.error('Vehicle table does\'nt be created', e.message))
 
-logTable
-  .sync()
-  .then(() => console.info('The Log table has be created'))
-  .catch((e) => console.error('Ops! Cant be create Log table', e.message))
+  await sellerTable
+    .sync()
+    .then(() => console.info('The Seller table has be created'))
+    .catch((e) => console.error('Ops! Cant be create seller table', e.message))
+
+  await logTable
+    .sync()
+    .then(() => console.info('The Log table has be created'))
+    .catch((e) => console.error('Ops! Cant be create Log table', e.message))
+}
+
+run()
